7fc196d33d87d62fc04b8a1857e43b67996897db
QuangNguyen2609 <quangnguyentechno@gmail.com>
2023-04-23 10:59:59 +0000

    fix accidentaly login + beautify

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,7 @@ connection.query('SELECT * FROM users', (error, results, fields) => {
 
 const users_email = [];
 const users_password = [];
+var cur_email = null;
 
 /* GET home page. */
 
@@ -36,10 +37,13 @@ app.get('/', function(req, res) {
 });
 
 app.get('/user', function(req, res) {
+  if (cur_email === null)
+    res.redirect('/');
   res.sendFile('/workspaces/Web-Project/public/user.html');
 });
 
 app.get('/login', function(req, res) {
+  cur_email = null;
   res.sendFile('/workspaces/Web-Project/public/login.html');
 });
 
@@ -52,12 +56,25 @@ app.get('/group', function(req, res) {
 });
 
 app.get('/group_user', function(req, res) {
+  if (cur_email === null)
+    res.redirect('/');
   res.sendFile('/workspaces/Web-Project/public/group_user.html');
+});
+
+app.get('/event', function(req, res) {
+  res.sendFile('/workspaces/Web-Project/public/event.html');
+});
+
+app.get('/event_user', function(req, res) {
+  if (cur_email === null)
+    res.redirect('/');
+  res.sendFile('/workspaces/Web-Project/public/event_user.html');
 });
 
 app.post('/login', function(req, res){
   var email = req.body.email;
   var password = req.body.password;
+  cur_email = email;
   if (users_email.includes(email) && users_password.includes(password))
     res.redirect('/user');
   else
