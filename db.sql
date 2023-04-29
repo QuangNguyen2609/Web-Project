c14170b5f9ebb824e8bafd798d59926490a879bb
tuanminh.nguyen@student.adelaide.edu.au <tuanminh.nguyen@student.adelaide.edu.au>
2023-04-29 07:05:12 +0000

    add db.sql

--- db.sql
+++ db.sql
@@ -1,136 +1,217 @@
-DROP DATABASE IF EXISTS wdc;
+-- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
+--
+-- Host: localhost    Database: wdc
+-- ------------------------------------------------------
+-- Server version	8.0.32-0ubuntu0.22.04.2
 
-CREATE DATABASE wdc;
-USE wdc;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
--- CREATE TABLE `Profile`(
---     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
---     `user_id` INT UNSIGNED,
---     `date_of_birth` DATE,
---     `phone` INT,
---     `school` VARCHAR(255) ,
---     `degree` VARCHAR(255),
---     `location` VARCHAR(255),
---     `hobbies` TEXT ,
---     `bio` TEXT,
---     `profile_picture` VARCHAR(255),
---     PRIMARY KEY (`id`)
--- );
+--
+-- Current Database: `wdc`
+--
 
--- ALTER TABLE
---     `Profile` ADD UNIQUE `profile_user_id_unique`(`user_id`);
--- ALTER TABLE
---     `Profile` ADD UNIQUE `profile_phone_unique`(`phone`);
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `wdc` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 
+USE `wdc`;
 
-CREATE TABLE `Event_Participation`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `event_id` INT UNSIGNED NOT NULL,
-    `user_id` INT UNSIGNED NOT NULL,
-    PRIMARY KEY(`id`),
-    UNIQUE KEY `event_participation_user_id_unique`(`user_id`)
-);
+--
+-- Table structure for table `Club_Members`
+--
 
-CREATE TABLE `Users`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(255) NOT NULL,
-    `password` VARCHAR(255) NOT NULL,
-    -- `profile_id` INT UNSIGNED NOT NULL DEFAULT 0,
-    `is_admin` TINYINT(1) NOT NULL DEFAULT 0,
-    `date_of_birth` DATE,
-    `phone` INT,
-    `school` VARCHAR(255) ,
-    `degree` VARCHAR(255),
-    `location` VARCHAR(255),
-    `hobbies` TEXT ,
-    `bio` TEXT,
-    `profile_picture` VARCHAR(255),
-    PRIMARY KEY (`id`)
-);
+DROP TABLE IF EXISTS `Club_Members`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Club_Members` (
+  `club_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int unsigned NOT NULL,
+  PRIMARY KEY (`club_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `clubs_id_foreign` FOREIGN KEY (`club_id`) REFERENCES `Clubs` (`id`),
+  CONSTRAINT `clubs_member_users_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- ALTER TABLE
---     `Users` ADD UNIQUE `users_email_unique`(`email`);
--- ALTER TABLE
---     `Users` ADD UNIQUE `users_profile_id_unique`(`profile_id`);
+--
+-- Dumping data for table `Club_Members`
+--
 
+LOCK TABLES `Club_Members` WRITE;
+/*!40000 ALTER TABLE `Club_Members` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Club_Members` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE `Events`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `club_id` INT UNSIGNED NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
-    `description` TEXT NOT NULL,
-    `category` VARCHAR(255) NOT NULL,
-    `location` VARCHAR(255) NOT NULL,
-    `start_time` DATETIME NOT NULL,
-    `end_time` DATETIME NOT NULL,
-    `creator_id` INT UNSIGNED NOT NULL,
-   PRIMARY KEY (`id`)
-);
+--
+-- Table structure for table `Club_Updates`
+--
 
-ALTER TABLE
-    `Events` ADD UNIQUE `events_club_id_unique`(`club_id`);
-ALTER TABLE
-    `Events` ADD UNIQUE `events_creator_id_unique`(`creator_id`);
+DROP TABLE IF EXISTS `Club_Updates`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Club_Updates` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `club_id` int NOT NULL,
+  `creator_id` int unsigned NOT NULL,
+  `text` text NOT NULL,
+  `time_created` datetime NOT NULL,
+  `is_public` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `club_updates_club_id_unique` (`club_id`),
+  UNIQUE KEY `club_updates_creator_id_unique` (`creator_id`),
+  CONSTRAINT `club_updates_club_id_foreign` FOREIGN KEY (`id`) REFERENCES `Clubs` (`id`),
+  CONSTRAINT `club_updates_creator_id_foreign` FOREIGN KEY (`creator_id`) REFERENCES `Users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-CREATE TABLE `Club_Updates`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `club_id` INT NOT NULL,
-    `creator_id` INT UNSIGNED NOT NULL,
-    `text` TEXT NOT NULL,
-    `time_created` DATETIME NOT NULL,
-    `is_public` TINYINT(1) NOT NULL,
-    PRIMARY KEY (`id`)
-);
+--
+-- Dumping data for table `Club_Updates`
+--
 
-ALTER TABLE
-    `Club_Updates` ADD UNIQUE `club_updates_club_id_unique`(`club_id`);
-ALTER TABLE
-    `Club_Updates` ADD UNIQUE `club_updates_creator_id_unique`(`creator_id`);
+LOCK TABLES `Club_Updates` WRITE;
+/*!40000 ALTER TABLE `Club_Updates` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Club_Updates` ENABLE KEYS */;
+UNLOCK TABLES;
 
-CREATE TABLE `Club_Members`(
-    `club_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `user_id` INT UNSIGNED NOT NULL,
-    PRIMARY KEY(`club_id`),
-    INDEX (`user_id`)
-);
+--
+-- Table structure for table `Clubs`
+--
 
-CREATE TABLE `Clubs`(
-    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(255) NOT NULL,
-    `description` TEXT NOT NULL,
-    `category` VARCHAR(255) NOT NULL,
-    `location` VARCHAR(255) NOT NULL,
-    `manager_id` INT UNSIGNED NOT NULL,
-    PRIMARY KEY(`id`)
-);
+DROP TABLE IF EXISTS `Clubs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Clubs` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `manager_id` int unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `clubs_manager_id_unique` (`manager_id`),
+  CONSTRAINT `clubs_manager_id_foreign` FOREIGN KEY (`manager_id`) REFERENCES `Users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Dumping data for table `Clubs`
+--
 
-ALTER TABLE
-    `Clubs` ADD UNIQUE `clubs_manager_id_unique`(`manager_id`);
+LOCK TABLES `Clubs` WRITE;
+/*!40000 ALTER TABLE `Clubs` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Clubs` ENABLE KEYS */;
+UNLOCK TABLES;
 
-ALTER TABLE
-     `Event_Participation` ADD CONSTRAINT `events_id_foreign` FOREIGN KEY(`event_id`) REFERENCES `Events`(`id`);
+--
+-- Table structure for table `Event_Participation`
+--
 
-ALTER TABLE
-    `Club_Updates` ADD CONSTRAINT `club_updates_club_id_foreign` FOREIGN KEY(`id`) REFERENCES `Clubs`(`id`);
+DROP TABLE IF EXISTS `Event_Participation`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Event_Participation` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `event_id` int unsigned NOT NULL,
+  `user_id` int unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `event_participation_user_id_unique` (`user_id`),
+  KEY `events_id_foreign` (`event_id`),
+  CONSTRAINT `events_id_foreign` FOREIGN KEY (`event_id`) REFERENCES `Events` (`id`),
+  CONSTRAINT `users_id_foreign` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-ALTER TABLE
-    `Club_Updates` ADD CONSTRAINT `club_updates_creator_id_foreign` FOREIGN KEY(`creator_id`) REFERENCES `Users`(`id`);
+--
+-- Dumping data for table `Event_Participation`
+--
 
-ALTER TABLE
-    `Event_Participation` ADD CONSTRAINT `users_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`);
+LOCK TABLES `Event_Participation` WRITE;
+/*!40000 ALTER TABLE `Event_Participation` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Event_Participation` ENABLE KEYS */;
+UNLOCK TABLES;
 
-ALTER TABLE
-    `Events` ADD CONSTRAINT `events_club_id_foreign` FOREIGN KEY(`club_id`) REFERENCES `Clubs`(`id`);
+--
+-- Table structure for table `Events`
+--
 
-ALTER TABLE
-    `Events` ADD CONSTRAINT `events_creator_id_foreign` FOREIGN KEY(`creator_id`) REFERENCES `Users`(`id`);
+DROP TABLE IF EXISTS `Events`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Events` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `club_id` int unsigned NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `start_time` datetime NOT NULL,
+  `end_time` datetime NOT NULL,
+  `creator_id` int unsigned NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `events_club_id_unique` (`club_id`),
+  UNIQUE KEY `events_creator_id_unique` (`creator_id`),
+  CONSTRAINT `events_club_id_foreign` FOREIGN KEY (`club_id`) REFERENCES `Clubs` (`id`),
+  CONSTRAINT `events_creator_id_foreign` FOREIGN KEY (`creator_id`) REFERENCES `Users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-ALTER TABLE
-    `Clubs` ADD CONSTRAINT `clubs_manager_id_foreign` FOREIGN KEY(`manager_id`) REFERENCES `Users`(`id`);
+--
+-- Dumping data for table `Events`
+--
 
-ALTER TABLE
-    `Club_Members` ADD CONSTRAINT `clubs_member_users_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`);
+LOCK TABLES `Events` WRITE;
+/*!40000 ALTER TABLE `Events` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Events` ENABLE KEYS */;
+UNLOCK TABLES;
 
-ALTER TABLE
-    `Club_Members` ADD CONSTRAINT `clubs_id_foreign` FOREIGN KEY(`club_id`) REFERENCES `Clubs`(`id`);
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Users` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `is_admin` tinyint(1) NOT NULL DEFAULT '0',
+  `date_of_birth` date DEFAULT NULL,
+  `phone` int DEFAULT NULL,
+  `school` varchar(255) DEFAULT NULL,
+  `degree` varchar(255) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `hobbies` text,
+  `bio` text,
+  `profile_picture` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+LOCK TABLES `Users` WRITE;
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+INSERT INTO `Users` VALUES (1,'quang@2003','123',0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(2,'minh@uoa','git',0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL),(3,'quan@123','123',0,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-04-29  6:58:08
