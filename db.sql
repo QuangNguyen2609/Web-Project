b2bfde994944308da1fa6f948ec99236fb1a76af
QuangNguyen2609 <quangnguyentechno@gmail.com>
2023-04-29 01:10:44 +0000

    add database init + insert profile to db

--- db.sql
+++ db.sql
@@ -1,62 +1,136 @@
--- MySQL dump 10.13  Distrib 8.0.32, for Linux (x86_64)
---
--- Host: localhost    Database: wdc
--- ------------------------------------------------------
--- Server version	8.0.32-0ubuntu0.22.04.2
+DROP DATABASE IF EXISTS wdc;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE wdc;
+USE wdc;
 
---
--- Current Database: `wdc`
---
+-- CREATE TABLE `Profile`(
+--     `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+--     `user_id` INT UNSIGNED,
+--     `date_of_birth` DATE,
+--     `phone` INT,
+--     `school` VARCHAR(255) ,
+--     `degree` VARCHAR(255),
+--     `location` VARCHAR(255),
+--     `hobbies` TEXT ,
+--     `bio` TEXT,
+--     `profile_picture` VARCHAR(255),
+--     PRIMARY KEY (`id`)
+-- );
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `wdc` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+-- ALTER TABLE
+--     `Profile` ADD UNIQUE `profile_user_id_unique`(`user_id`);
+-- ALTER TABLE
+--     `Profile` ADD UNIQUE `profile_phone_unique`(`phone`);
 
-USE `wdc`;
 
---
--- Table structure for table `users`
---
+CREATE TABLE `Event_Participation`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `event_id` INT UNSIGNED NOT NULL,
+    `user_id` INT UNSIGNED NOT NULL,
+    PRIMARY KEY(`id`),
+    UNIQUE KEY `event_participation_user_id_unique`(`user_id`)
+);
 
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` int unsigned NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) NOT NULL,
-  `email` varchar(50) NOT NULL,
-  `age` int DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+CREATE TABLE `Users`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    -- `profile_id` INT UNSIGNED NOT NULL DEFAULT 0,
+    `is_admin` TINYINT(1) NOT NULL DEFAULT 0,
+    `date_of_birth` DATE,
+    `phone` INT,
+    `school` VARCHAR(255) ,
+    `degree` VARCHAR(255),
+    `location` VARCHAR(255),
+    `hobbies` TEXT ,
+    `bio` TEXT,
+    `profile_picture` VARCHAR(255),
+    PRIMARY KEY (`id`)
+);
 
---
--- Dumping data for table `users`
---
+-- ALTER TABLE
+--     `Users` ADD UNIQUE `users_email_unique`(`email`);
+-- ALTER TABLE
+--     `Users` ADD UNIQUE `users_profile_id_unique`(`profile_id`);
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'Alice','alice@example.com',25,'2023-04-23 03:48:59'),(2,'Bob','bob@example.com',30,'2023-04-23 03:48:59'),(3,'Charlie','charlie@example.com',35,'2023-04-23 03:48:59'),(4,'David','david@example.com',NULL,'2023-04-23 03:48:59'),(5,'Eve','eve@example.com',28,'2023-04-23 03:48:59');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+CREATE TABLE `Events`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `club_id` INT UNSIGNED NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `description` TEXT NOT NULL,
+    `category` VARCHAR(255) NOT NULL,
+    `location` VARCHAR(255) NOT NULL,
+    `start_time` DATETIME NOT NULL,
+    `end_time` DATETIME NOT NULL,
+    `creator_id` INT UNSIGNED NOT NULL,
+   PRIMARY KEY (`id`)
+);
 
--- Dump completed on 2023-04-23  3:49:54
+ALTER TABLE
+    `Events` ADD UNIQUE `events_club_id_unique`(`club_id`);
+ALTER TABLE
+    `Events` ADD UNIQUE `events_creator_id_unique`(`creator_id`);
+
+CREATE TABLE `Club_Updates`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `club_id` INT NOT NULL,
+    `creator_id` INT UNSIGNED NOT NULL,
+    `text` TEXT NOT NULL,
+    `time_created` DATETIME NOT NULL,
+    `is_public` TINYINT(1) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+ALTER TABLE
+    `Club_Updates` ADD UNIQUE `club_updates_club_id_unique`(`club_id`);
+ALTER TABLE
+    `Club_Updates` ADD UNIQUE `club_updates_creator_id_unique`(`creator_id`);
+
+CREATE TABLE `Club_Members`(
+    `club_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `user_id` INT UNSIGNED NOT NULL,
+    PRIMARY KEY(`club_id`),
+    INDEX (`user_id`)
+);
+
+CREATE TABLE `Clubs`(
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(255) NOT NULL,
+    `description` TEXT NOT NULL,
+    `category` VARCHAR(255) NOT NULL,
+    `location` VARCHAR(255) NOT NULL,
+    `manager_id` INT UNSIGNED NOT NULL,
+    PRIMARY KEY(`id`)
+);
+
+
+ALTER TABLE
+    `Clubs` ADD UNIQUE `clubs_manager_id_unique`(`manager_id`);
+
+ALTER TABLE
+     `Event_Participation` ADD CONSTRAINT `events_id_foreign` FOREIGN KEY(`event_id`) REFERENCES `Events`(`id`);
+
+ALTER TABLE
+    `Club_Updates` ADD CONSTRAINT `club_updates_club_id_foreign` FOREIGN KEY(`id`) REFERENCES `Clubs`(`id`);
+
+ALTER TABLE
+    `Club_Updates` ADD CONSTRAINT `club_updates_creator_id_foreign` FOREIGN KEY(`creator_id`) REFERENCES `Users`(`id`);
+
+ALTER TABLE
+    `Event_Participation` ADD CONSTRAINT `users_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`);
+
+ALTER TABLE
+    `Events` ADD CONSTRAINT `events_club_id_foreign` FOREIGN KEY(`club_id`) REFERENCES `Clubs`(`id`);
+
+ALTER TABLE
+    `Events` ADD CONSTRAINT `events_creator_id_foreign` FOREIGN KEY(`creator_id`) REFERENCES `Users`(`id`);
+
+ALTER TABLE
+    `Clubs` ADD CONSTRAINT `clubs_manager_id_foreign` FOREIGN KEY(`manager_id`) REFERENCES `Users`(`id`);
+
+ALTER TABLE
+    `Club_Members` ADD CONSTRAINT `clubs_member_users_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`);
+
+ALTER TABLE
+    `Club_Members` ADD CONSTRAINT `clubs_id_foreign` FOREIGN KEY(`club_id`) REFERENCES `Clubs`(`id`);
